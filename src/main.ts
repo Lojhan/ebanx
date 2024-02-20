bb128723d412c8d7fae0450e83f482b97470176d
Lojhan <viniciuslojhan@gmail.com>
2024-02-20 09:18:58 -0300

    config: changed port

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { serve, Server } from 'bun'
 import { AccountsModule } from './accounts'
 
 const app = serve({
-    port: 3000,
+    port: 8080,
     async fetch(this, request: Request, server: Server) { 
         const response = await AccountsModule.init(request)
         if (response) return response;
         return new Response('Not found', { status: 404 })
     },
-    
+
 })
